d56922db375d9267ec9276165f3c6dcd78cb7e88
Vivekanandhan Perumal <vivekanandhan87@gmail.com>
2020-06-15 16:11:17 -0400

    uncomment the register method register user

--- routes/users.js
+++ routes/users.js
@@ -10,29 +10,29 @@ router.get("/:me", auth, async (req, res) => {
   res.send(user);
 });
 
-router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
+// router.post("/", async (req, res) => {
+//   const { error } = validate(req.body);
 
-  if (error) {
-    // 400 bad request
-    res.status(400).send(error.details[0].message);
-    return;
-  }
+//   if (error) {
+//     // 400 bad request
+//     res.status(400).send(error.details[0].message);
+//     return;
+//   }
 
-  let user = await User.findOne({ email: req.body.email });
+//   let user = await User.findOne({ email: req.body.email });
 
-  if (user) {
-    res.status(400).send("User already registered");
-    return;
-  }
+//   if (user) {
+//     res.status(400).send("User already registered");
+//     return;
+//   }
 
-  user = new User(_.pick(req.body, ["name", "email", "password"]));
+//   user = new User(_.pick(req.body, ["name", "email", "password"]));
 
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
+//   const salt = await bcrypt.genSalt(10);
+//   user.password = await bcrypt.hash(user.password, salt);
 
-  await user.save();
-  res.send(_.pick(user, ["_id", "name", "email"]));
-});
+//   await user.save();
+//   res.send(_.pick(user, ["_id", "name", "email"]));
+// });
 
 module.exports = router;
